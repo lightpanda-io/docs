6d3af96fb45030293ba06b901ec69d2a14391349
Pierre Tachoire <pierre@lightpanda.io>
2025-10-21 10:52:32 +0200

    change wording

--- src/content/quickstart/_meta.ts
+++ src/content/quickstart/_meta.ts
@@ -2,8 +2,8 @@ import type { MetaRecord } from 'nextra'
 
 const meta: MetaRecord = {
   'installation-and-setup': 'Installation and setup',
-  'what-is-lightpanda-and-your-first-test': 'Your first test',
-  'build-your-first-scraper': 'Build your first scraper',
+  'your-first-test': 'Your first test',
+  'build-your-first-extraction-script': 'Build your first data extraction script',
 }
 
 export default meta
