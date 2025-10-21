22ebfd055fbc44c669a888a2853a40547b2f4a28
Pierre Tachoire <pierre@lightpanda.io>
2025-10-21 10:44:05 +0200

    adjust title

--- src/content/quickstart/_meta.ts
+++ src/content/quickstart/_meta.ts
@@ -2,7 +2,7 @@ import type { MetaRecord } from 'nextra'
 
 const meta: MetaRecord = {
   'installation-and-setup': 'Installation and setup',
-  'what-is-lightpanda-and-your-first-test': 'What is lightpanda and your first test',
+  'what-is-lightpanda-and-your-first-test': 'Your first test',
   'build-your-first-scraper': 'Build your first scraper',
 }
 
