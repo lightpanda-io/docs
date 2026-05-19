ba6eed023d1e26354a805152eb6992d6153f4309
Céline Debled <comptes.98@gmail.com>
2026-05-19 22:39:24 +0200

    split installation page into one-liner, docker and nightly-builds subpages

---
+++ src/content/run-locally/installation/_meta.ts
@@ -0,0 +1,10 @@
+import type { MetaRecord } from 'nextra'
+
+const meta: MetaRecord = {
+  'one-liner': 'One-liner',
+  docker: 'Docker',
+  'package-managers': 'Package manager (npm, brew)',
+  'nightly-builds': 'Nightly builds',
+}
+
+export default meta
