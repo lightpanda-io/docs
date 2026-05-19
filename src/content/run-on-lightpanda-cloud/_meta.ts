39a62c07122a024f9f5e96686cb1091bbfd12748
Céline Debled <comptes.98@gmail.com>
2026-05-19 22:58:40 +0200

    remove tools section from run-on-lightpanda-cloud

--- src/content/run-on-lightpanda-cloud/_meta.ts
+++ src/content/run-on-lightpanda-cloud/_meta.ts
@@ -2,12 +2,6 @@ import type { MetaRecord } from 'nextra'
 
 const meta: MetaRecord = {
   'getting-started': 'Getting started',
-  tools: {
-    title: 'Tools',
-    theme: {
-      collapsed: false,
-    },
-  },
 }
 
 export default meta
