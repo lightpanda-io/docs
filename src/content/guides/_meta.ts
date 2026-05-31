3bf6e878a3a7fde484535da4b25928e2d95a9316
Celine Debled <comptes.98@gmail.com>
2026-05-31 09:02:47 +0200

    fix some links, nextra imports and restructuration content

--- src/content/guides/_meta.ts
+++ src/content/guides/_meta.ts
@@ -2,7 +2,7 @@ import type { MetaRecord } from 'nextra'
 
 const meta: MetaRecord = {
   'retrieve-an-html-webpage': 'Retrieve an HTML webpage',
-  'markdown-axtree': 'Get a markdown version',
+  'markdown-axtree': 'Get Markdown and AX Tree',
   'interact-with-a-webpage': 'Interact with a webpage',
   'use-stagehand': 'Use Stagehand',
   'use-hermes': 'Use Hermes Agent',
